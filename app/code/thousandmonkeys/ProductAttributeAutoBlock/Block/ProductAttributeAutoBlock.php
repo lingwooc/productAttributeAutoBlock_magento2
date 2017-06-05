88dcd1b001cf0d5bcb48f83df04e1a907922abef
Chris Lingwood <chris@thousandmonkeys.com>
2017-06-05 12:43:31 +0100

    working

---
+++ app/code/thousandmonkeys/ProductAttributeAutoBlock/Block/ProductAttributeAutoBlock.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace thousandmonkeys\ProductAttributeAutoBlock\Block;
+
+use Magento\Catalog\Block\Product\Context;
+use Magento\Framework\Stdlib\ArrayUtils;
+
+class ProductAttributeAutoBlock extends \Magento\Catalog\Block\Product\View\abstractView {
+
+	protected $_template = 'attributeBlock.phtml';
+	protected $_scopeConfig;
+
+    /**
+     * CustomDescription constructor.
+     *
+     * @param Context $context
+     * @param ArrayUtils $arrayUtils
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        ArrayUtils $arrayUtils,
+        array $data
+    ) {
+		$this->_scopeConfig = $context->getScopeConfig();
+        parent::__construct($context, $arrayUtils, $data);
+    }
+
+	public function getAttributeID()
+	{
+		return $this->_scopeConfig->getValue('ProductAttributeAutoBlock/attribute/attributeid', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+	}
+
+	public function isSetup(){
+		return !is_null($this->getAttributeID());
+	}
+
+	public function getBlockIDs(){
+		$attID = $this->getAttributeID();
+		$product = $this->getProduct();
+
+		$blockCodes = array();
+
+		$values = $product->getResource()->getAttribute($attID)->getFrontend()->getValue($product); 
+		foreach (explode(',',$values) as $value)
+		    $blockCodes[] = $attID.'_'.trim($value);
+
+		return $blockCodes;
+	}
+}
